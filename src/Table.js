f2d312422ff2d838c1734423567c8b4c79817bfc
Matthew Bronstein <mbronstein1@gmail.com>
2023-02-15 11:13:41 -0600

    added simple styling to test imported code from sandbox

--- src/Table.js
+++ src/Table.js
@@ -1,13 +1,13 @@
-import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-import * as opportunities from "./opportunities.json";
+import * as opportunities from './opportunities.json';
 
 export default function BasicTable() {
   /**
@@ -16,41 +16,40 @@ export default function BasicTable() {
   const data = opportunities.default;
 
   function handleRowClick(event, row) {
-    console.log("row", row);
+    console.log('row', row);
   }
 
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table sx={{ minWidth: 650 }} aria-label='simple table'>
         <TableHead>
-          <TableRow>
-            <TableCell align="left">Opp Name</TableCell>
-            <TableCell align="left">Opp Stage</TableCell>
-            <TableCell align="right">Rep Probability</TableCell>
-            <TableCell align="right">PX Probability</TableCell>
-            <TableCell align="left">PX Tier</TableCell>
-            <TableCell align="right">Amount</TableCell>
-            <TableCell align="left">Product</TableCell>
-            <TableCell align="left">Sales Rep</TableCell>
+          <TableRow sx={{ borderBottom: '5px solid black' }}>
+            <TableCell align='left'>Opp Name</TableCell>
+            <TableCell align='left'>Opp Stage</TableCell>
+            <TableCell align='right'>Rep Probability</TableCell>
+            <TableCell align='right'>PX Probability</TableCell>
+            <TableCell align='left'>PX Tier</TableCell>
+            <TableCell align='right'>Amount</TableCell>
+            <TableCell align='left'>Product</TableCell>
+            <TableCell align='left'>Sales Rep</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row) => (
+          {data.map(row => (
             <TableRow
-              onClick={(event) => handleRowClick(event, row)}
+              onClick={event => handleRowClick(event, row)}
               key={row.oppId}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
+              sx={{ '&:last-child td, &:last-child th': { border: 0 }, '&:nth-of-type(even)': { backgroundColor: 'lightgrey' } }}>
+              <TableCell component='th' scope='row'>
                 {row.oppName}
               </TableCell>
-              <TableCell align="left">{row.stage}</TableCell>
-              <TableCell align="right">{row.repProbability}</TableCell>
-              <TableCell align="right">{row.pilytixProbability}</TableCell>
-              <TableCell align="left">{row.pilytixTier}</TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
-              <TableCell align="left">{row.product}</TableCell>
-              <TableCell align="left">{row.salesRepName}</TableCell>
+              <TableCell align='left'>{row.stage}</TableCell>
+              <TableCell align='right'>{row.repProbability}</TableCell>
+              <TableCell align='right'>{row.pilytixProbability}</TableCell>
+              <TableCell align='left'>{row.pilytixTier}</TableCell>
+              <TableCell align='right'>{row.amount}</TableCell>
+              <TableCell align='left'>{row.product}</TableCell>
+              <TableCell align='left'>{row.salesRepName}</TableCell>
             </TableRow>
           ))}
         </TableBody>
