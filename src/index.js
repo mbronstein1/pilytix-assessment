06a7c308494f07a25d3a62906f265c0dbbabc2ef
Matthew Bronstein <mbronstein1@gmail.com>
2023-02-16 15:16:01 -0600

    added confirmation comments for assessment

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-/* Time invested so far
-    Research/planning -> 1.5 hours
-    Coding -> 4 hours
+/* Time invested
+    Research/planning -> 2 hours
+    Coding -> 6 hours
+    Cleanup -> 1 hour
+*/
+
+/* 
+Matthew Bronstein
+mbronstein1@gmail.com
+I testify that I completed this assessment without any help from
+any outside sources other than resources I found via the web.
 */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
